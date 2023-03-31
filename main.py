2e3ed6d2dd603d9cc91754c32a0837b118f1ac1d
Vitaliy Kuzhil <vitaliy.kuzhil2018@gmail.com>
2023-03-31 13:12:14 +0300

    Minor fix in main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Vitaliy')
+    print_hi('Vitaliy Kuzhil')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
